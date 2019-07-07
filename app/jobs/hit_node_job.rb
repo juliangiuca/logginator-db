d4270fbb5b22d0ccf0ccb0dee370d5c271ca227d
Julian Giuca <julian@newrelic.com>
2019-07-07 10:17:48 -0700

    good enough

--- app/jobs/hit_node_job.rb
+++ app/jobs/hit_node_job.rb
@@ -1,13 +1,15 @@
 class HitNodeJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
+  def perform(args={})
     logger.info("hit node job was called with args #{args}")
-    url = args['url']
+    url = args["url"]
     if url
-      response = HTTParty.get("http://localhost:4000/?url=#{args['url']}")
-      logger.info(response.body)
+      logger.info("requesting #{url}")
+      response = HTTParty.get("http://localhost:4000/?url=#{url}")
+      logger.info("A response of #{response.body} from #{url}")
     else
       logger.info('No URL provided. Did not try')
+    end
   end
 end
--- app/models/website.rb
+++ app/models/website.rb
@@ -1,9 +1,9 @@
 class Website < ApplicationRecord
-  after_save :re_cron
+  after_create :re_cron
 
   private
   def re_cron
-    logger.info('re_cron called')
+    logger.info("re_cron called from #{self}")
     Sidekiq::Cron::Job.create(name: 'Hard worker - every 1min', cron: '*/1 * * * *', class: 'HitNodeJob', args: {url: self.url}) # execute at every 1 minutes
   end
 end
